e9cef6bc50627b4c0813f7497f6e9df0b32fd0a6
Git-crypto-rgb <Q2108268270@yahoo.com>
2025-11-30 23:45:42 -0500

    add math-config.js to support latex systematically

---
+++ math-config.js
@@ -0,0 +1,17 @@
+// math-config.js
+window.MathJax = {
+  tex: {
+    inlineMath: [['$', '$'], ['\\(', '\\)']]
+  },
+  svg: {
+    fontCache: 'global'
+  }
+};
+
+// 动态加载 MathJax 库
+(function () {
+  var script = document.createElement('script');
+  script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js';
+  script.async = true;
+  document.head.appendChild(script);
+})();
